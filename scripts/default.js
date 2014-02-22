a7de1249fbb94f506b5b7cefb2547b7916728c51
torelechuck <toresaue@gmail.com>
2014-02-22 19:32:51 +0100

    førsteutkast brett med brikker

--- scripts/default.js
+++ scripts/default.js
@@ -1,27 +1,76 @@
 $(document).ready(function(){
-    var board = createBoard();
+    var board = initGame();
     drawBoard(board);
 })
 
 function drawBoard(board) {
-    for (var row = 0; row < board.length; row++) {
-        for (var col = 0; col < board.length; col++) {
+    for (var rank = board.length - 1; rank >= 0; rank--) {
+        for (var file = 0; file < board.length; file++) {
             var colorClass = 'whiteSquare';
-            if( col%2 - row%2 === 0 ) colorClass = 'blackSquare';
-            var $square = $('<div id ="' + board[row][col]  + '" class ="' + colorClass + '"></div >'); 
-            $square.appendTo($('#board'));
+            if (file%2 - rank%2 === 0) colorClass = 'blackSquare';
+            var square = board[rank][file];
+            var $squareDiv = $('<div id ="' + square.name  + '" class ="' + colorClass + '"></div >'); 
+            $squareDiv.appendTo($('#board'));
+            var piece = square.piece;
+            if (piece) {
+                var pieceClass = piece.color + piece.type;
+                var $pieceImg = $('<img class="' + pieceClass + '" src ="pieces/' + pieceClass + '.svg"/>');
+                $pieceImg.appendTo($squareDiv);
+            };            
         };
     };
 }
 
-function createBoard() {
-    var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+function initGame() {
+    var board = initBoard();
+    initPieces(board);
+    return board;
+}
+
+function initBoard() {
+    var fileLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
     var board = new Array(8);
-    for (var row = 0; row < 8; row++) {
-        board[row] = new Array(8);
-        for (var col = 0; col < 8; col++) {
-            board[row][col] = letters[col] + (row + 1);
+    for (var rank = 0; rank < board.length; rank++) {
+        board[rank] = new Array(8);
+        for (var file = 0; file < board[rank].length; file++) {
+            board[rank][file] = {
+                name: fileLetters[file] + (rank + 1),
+                piece: null
+            };
         };
     };
     return board;
 }
+
+function initPieces(board) {
+    initNonPawns(board[0], "white");
+    initPawns(board[1], "white");
+    initPawns(board[6], "black");
+    initNonPawns(board[7], "black");
+}
+
+function initPawns(rank, color) {
+    for (var i = 0; i < rank.length; i++) {
+        rank[i].piece = {
+            type: "pawn",
+            prefix: "",
+            color: color
+        };
+    };
+}
+
+function initNonPawns(rank, color) {
+    var fileTypes = ["rook", "night", "bishop", "queen", "king", "bishop", "night", "rook"];
+    for (var i = 0; i < rank.length; i++) {
+        rank[i].piece = {
+            type: fileTypes[i],
+            prefix: fileTypes[i].substr(0,1).toUpperCase(),
+            color: color
+        };
+    };
+}
+
+
+
+
+
